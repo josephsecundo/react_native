0ea1556b5594ed788567509cd794d1f1185c6e96
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-03-08 20:41:15 -0300

    aula 08/03 - useState

--- src/ex_3/index.js
+++ src/ex_3/index.js
@@ -1,10 +1,37 @@
+import React, {useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native-web';
 
 import styles from './styles';
+
 export default function Exemplo_3 (){
+
+    const [numero, setNumero] = useState(0)};
+
+    function Ola (nome) {
+        alert(`Ola ${nome}`);
+
+        function incremento(){
+            numero++;
+            // console.log(numero);
+        }
+    }
     return(
-        <view style={styles.container}>
-            <text>Exemplo 3</text>
-        </view>
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Exemplo 3</Text>
+
+            <TouchableOpacity style={styles.botao}
+            onPress={() => Ola('José')}
+            >
+                <Text style={styles.txtbotao}>Botao</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.titulo}> Valor: {numero}</Text>
+
+                <TouchableOpacity style={styles.botao}
+                onPress={() => incremento()}
+            >
+                <Text style={styles.txtbotao}>+ 1</Text>
+                </TouchableOpacity>
+        </View>
     )
 }
--- src/ex_3/styles.js
+++ src/ex_3/styles.js
@@ -6,7 +6,29 @@ const styles=StyleSheet.create({
         backgroundColor: '#f5f5f5',
         width: '100%',
         alignItems: 'center',
+        padding: 8,
+        borderRadius: 20,
     },
+
+    titulo: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        margin: 8,
+        marginBottom: 20,
+    },
+    botao:{
+        width: '70%',
+        padding: 16,
+        backgroundColor: '#000',
+        color: '#f5f5f5',
+        fontSize: 20,
+        alignItems: 'center',
+        borderRadius: 20,
+    },
+    txtbotao:{
+        color: '#f5f5f5',
+        fontSize: 20,
+    }
 })
 
 export default styles;
