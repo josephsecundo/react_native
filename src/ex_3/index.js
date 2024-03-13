827b8b36193078db12aed31bc74885ebcdecbc1d
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-03-13 20:25:40 -0300

    Atividade 3 - 2024_03_13

--- src/ex_3/index.js
+++ src/ex_3/index.js
@@ -1,37 +1,40 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native-web';
+import { useState } from 'react';
+import { View, Text, Pressable  } from 'react-native';
 
 import styles from './styles';
 
-export default function Exemplo_3 (){
+export default function Exemplo_3 () {
 
-    const [numero, setNumero] = useState(0)};
+    const [numero, setNumero] = useState(0);
 
-    function Ola (nome) {
+    function Ola(nome) {
         alert(`Ola ${nome}`);
-
-        function incremento(){
-            numero++;
-            // console.log(numero);
-        }
     }
-    return(
+
+    function incremento() {
+        const inc = numero + 1;
+        setNumero(inc);
+        console.log(numero);
+    }
+
+    return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 3</Text>
 
-            <TouchableOpacity style={styles.botao}
-            onPress={() => Ola('José')}
+            <Pressable  style={styles.botao}
+                onPress={() => Ola('José')}
             >
                 <Text style={styles.txtbotao}>Botao</Text>
-                </TouchableOpacity>
+            </Pressable >
 
-                <Text style={styles.titulo}> Valor: {numero}</Text>
+            <Text style={styles.titulo}>Valor: {numero}</Text>
 
-                <TouchableOpacity style={styles.botao}
+            <Pressable  style={styles.botao}
                 onPress={() => incremento()}
             >
                 <Text style={styles.txtbotao}>+ 1</Text>
-                </TouchableOpacity>
+            </Pressable >
         </View>
-    )
+    );
+
 }
